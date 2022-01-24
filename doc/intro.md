3e7ebd94f3b317264aab909645d1f42c2cc7951c
tom <tom@that-place>
2022-01-23 23:29:17 -0600

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dataspectest
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dataspectest/core_test.clj
@@ -0,0 +1,7 @@
+(ns dataspectest.core-test
+  (:require [clojure.test :refer :all]
+            [dataspectest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
